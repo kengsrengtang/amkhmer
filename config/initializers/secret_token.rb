fd6854fd414b00f88a6c59fdaaf396f1cb99d65c
ubuntu <ubuntu@ubuntu-ThinkPad-T410.(none)>
2012-11-18 13:36:49 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amkhmer::Application.config.secret_token = '507a013d51ff6bff95d36860905289511ac58f02727e9695603e0be62f1a2880d7d883843bfd8d75b8918dabdacae22115228e54be1726133108903eb8ea683b'
