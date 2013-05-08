662a8afbc3df4c71093d9e30dae67c991a117b77
Apple <Apple@Apples-MacBook-Pro.local>
2013-05-08 22:11:34 +0700

    modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    #put File.read("config/database.example.yml", "#{shared_path}/config/database.yml")
+    put File.read("config/database.example.yml", "#{shared_path}/config/database.yml")
     puts "Now edit the config fiels in #{shared_path}."
   end
 
